be84dd17c2f0a9ac7bc7ab8c33dcc0db2cfbe2af
Valentin Duffet <vavadu07@gmail.com>
2023-03-17 14:32:38 +0100

    add topbar

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,11 +1,14 @@
 import styles from "./topbar.module.scss";
-// import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Topbar = () => {
   return (
     <div className={styles["container"]}>
-      <h1>Je suis la topbar</h1>
-      {/* <NavLink to='about'>A propos</NavLink> */}
+      <img src={logo} alt='logo' />
+      <div className={styles["container-nav"]}>
+        <h2>Accueil</h2>
+        <h2>A Propos</h2>
+      </div>
     </div>
   );
 };
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,12 +1,6 @@
 import styles from "./home.module.scss";
-import { NavLink } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div className={styles["container"]}>
-      <h1>Je suis la home-page</h1>
-      <NavLink to='/about'>Go a propos</NavLink>
-    </div>
-  );
+  return <div className={styles["container"]}></div>;
 };
 export default Home;
